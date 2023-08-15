2843f216fcaddd722958e433fc19bb5af09915fc
Kymani Stephens <kymanist00@gmail.com>
2023-08-15 13:31:53 -0700

    add function to update color of div

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const canvas = document.querySelector('#canvas')
-const rangeInput = document.querySelector('#myRange') 
-let gridSize = rangeInput.value 
+const rangeInput = document.querySelector('#myrange') 
+const colorPicker = document.querySelector('#colorpicker') 
+let gridSize = rangeInput.value
+let currentColor = 'black'
 
 
 // updates the gridSize variable to be equal to slider input
-rangeInput.addEventListener("input", (e) => gridSize = e.target.value)
-
+rangeInput.addEventListener("input", (e) => gridSize = e.target.value) 
 // runs createGrid function everytime the value of the rangeslider changes
 rangeInput.addEventListener("input", () => createGrid(gridSize))
+
+// updates the currentColor variable to be equal to picker input
+colorPicker.addEventListener("input", (e) => currentColor = e.target.value)   
+
+
+function changeColor(e) {
+  e.target.style.backgroundColor = `${currentColor}` 
+}
+
+
 
 function createGrid(gridSize) {
   canvas.replaceChildren() // remove all canvas divs before generating new ones
@@ -16,6 +27,7 @@ function createGrid(gridSize) {
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     const div = document.createElement('div')
+    div.addEventListener('click', changeColor)
     canvas.appendChild(div)
   }
 }
