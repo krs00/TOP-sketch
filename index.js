6c52f72ecc4d2e81b648e7c7d17d8d3636b6b73c
Kymani Stephens <kymanist00@gmail.com>
2023-08-15 12:04:08 -0700

    add comments

--- index.js
+++ index.js
@@ -9,19 +9,16 @@ rangeInput.addEventListener("input", (e) => gridSize = e.target.value)
 // runs createGrid function everytime the value of the rangeslider changes
 rangeInput.addEventListener("input", () => createGrid(gridSize))
 
-
-
 function createGrid(gridSize) {
-
-  canvas.replaceChildren() 
+  canvas.replaceChildren() // remove all canvas divs before generating new ones
   canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
   canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
 
   for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement('div') 
-    div.style.backgroundColor = 'blue'
+    const div = document.createElement('div')
     canvas.appendChild(div)
   }
 }
+
 
 createGrid(8)
