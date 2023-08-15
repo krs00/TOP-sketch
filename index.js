281a1bb1793a6ee3130f75139fb6da211d8fe558
Kymani Stephens <kymanist00@gmail.com>
2023-08-15 12:02:20 -0700

    add dynamic grid creation tied to slider value

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const canvas = document.querySelector('#canvas')
+const rangeInput = document.querySelector('#myRange') 
+let gridSize = rangeInput.value 
+
+
+// updates the gridSize variable to be equal to slider input
+rangeInput.addEventListener("input", (e) => gridSize = e.target.value)
+
+// runs createGrid function everytime the value of the rangeslider changes
+rangeInput.addEventListener("input", () => createGrid(gridSize))
 
 
 
 function createGrid(gridSize) {
 
+  canvas.replaceChildren() 
   canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
   canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
 
-
-  for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement('div')
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement('div') 
+    div.style.backgroundColor = 'blue'
     canvas.appendChild(div)
   }
 }
 
-
-createGrid(32);
+createGrid(8)
